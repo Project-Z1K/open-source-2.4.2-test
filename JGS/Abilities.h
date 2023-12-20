b66b7b1b34d8cd39cc7f00668cf57307b77d658f
Project-Z1K <149071205+Project-Z1K@users.noreply.github.com>
2023-12-20 15:57:27 +0100

    Fixed Abilities

--- JGS/Abilities.h
+++ JGS/Abilities.h
@@ -2,31 +2,21 @@
 
 namespace Abilities
 {
-    FGameplayAbilitySpecHandle(*GiveAbility)(UAbilitySystemComponent*, FGameplayAbilitySpecHandle* outHandle, FGameplayAbilitySpec);
+	void GrantGameplayAbility(AFortPlayerPawn* InPawn, UClass* InGameplayAbility)
+	{
+		auto AbilitySystemComponent = InPawn->AbilitySystemComponent;
 
-    static void GrantGameplayAbility(APlayerPawn_Athena_C* TargetPawn, UClass* GameplayAbilityClass)
-    {
-        auto AbilitySystemComponent = TargetPawn->AbilitySystemComponent;
+		auto DefaultGameplayEffect = UObject::FindObject<UGameplayEffect>("GE_Athena_PurpleStuff_C GE_Athena_PurpleStuff.Default__GE_Athena_PurpleStuff_C");
+		if (DefaultGameplayEffect) {
+			DefaultGameplayEffect->GrantedAbilities.operator[](0).Ability = InGameplayAbility;
+			DefaultGameplayEffect->GrantedAbilities.operator[](0).RemovalPolicy = EGameplayEffectGrantedAbilityRemovePolicy::DoNothing;
+			DefaultGameplayEffect->GrantedAbilities.operator[](0).Level = 1.0;
+			DefaultGameplayEffect->DurationPolicy = EGameplayEffectDurationType::Infinite;
 
-        if (!AbilitySystemComponent)
-            return;
-
-        auto GenerateNewSpec = [&]() -> FGameplayAbilitySpec
-            {
-                FGameplayAbilitySpecHandle Handle{ rand() };
-
-            };
-
-        auto Spec = GenerateNewSpec();
-
-        for (int i = 0; i < AbilitySystemComponent->ActivatableAbilities.Items.Num(); i++)
-        {
-            auto& CurrentSpec = AbilitySystemComponent->ActivatableAbilities.Items[i];
-
-            if (CurrentSpec.Ability == Spec.Ability)
-                return;
-        }
-
-        auto Handle = GiveAbility(AbilitySystemComponent, &Spec.Handle, Spec);
-    }
+			auto EffectClass = UObject::FindClass("BlueprintGeneratedClass GE_Athena_PurpleStuff.GE_Athena_PurpleStuff_C");
+			if (EffectClass) {
+				AbilitySystemComponent->BP_ApplyGameplayEffectToSelf(EffectClass, 1.0, FGameplayEffectContextHandle());
+			}
+		}
+	}
 }
